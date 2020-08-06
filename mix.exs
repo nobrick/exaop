d5dcdefae1b210f604b7bb1ba16a4574e1a56467
Ming Qu <qucool@gmail.com>
2020-08-06 20:41:44 +0800

    Add GitHub actions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exaop.MixProject do
     [
       app: :exaop,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       description: description(),
       start_permanent: Mix.env() == :prod,
       package: package(),
